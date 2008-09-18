7c9533a01e265c19f04b1908391301c34c3dcfcf
Geoff Cant <nem@erlang.geek.nz>
2008-09-18 13:36:50 +0200

    Adding information for EDBI on how to use backend drivers.

---
+++ src/edbi_driver.erl
@@ -0,0 +1,42 @@
+%%%-------------------------------------------------------------------
+%% @copyright Geoff Cant
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc Driver utilities
+%% @end
+%%%-------------------------------------------------------------------
+-module(edbi_driver).
+
+%% API
+-export([connect/2
+         ,connect_mfa/2
+         ,terminate/2]).
+
+%%====================================================================
+%% API
+%%====================================================================
+
+connect(pgsql, Options) ->
+    pgsql:connect(Options);
+connect(dummy, Options) ->
+    edbi_dummy:connect(Options);
+connect(Driver, _) ->
+    erlang:error({unknown_driver, Driver}).
+
+connect_mfa(pgsql, Options) ->
+    {pgsql, connect_link, Options};
+connect_mfa(dummy, Options) ->
+    {edbi_dummy, connect, Options};
+connect_mfa(Driver, _) ->
+    erlang:error({unknown_driver, Driver}).
+
+terminate(pgsql, Pid) ->
+    pgsql:terminate(Pid);
+terminate(dummy, Pid) ->
+    Pid ! shutdown;
+terminate(Driver, _) ->
+    erlang:error({unknown_driver, Driver}).
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
